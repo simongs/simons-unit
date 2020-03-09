2ab648970df1f9902485c873ac25609b4e0c144d
chopokmado <chopokmado@gmail.com>
2020-03-09 21:59:58 +0900

    간단한 테스트 진행 - 리플렉션을 적용해서 진행

---
+++ src/main/java/PureJavaFirstTest.java
@@ -0,0 +1,16 @@
+package main.java;
+
+public class PureJavaFirstTest {
+
+	/**
+	 * 	첫번째 순수 자바로만 만드는 테스트
+	 */
+	public static void main(String args[]) {
+		WasRun wasRun = new WasRun("testMethod");
+
+		System.out.println(wasRun.wasRun); // false
+		wasRun.run();
+
+		System.out.println(wasRun.wasRun); // true
+	}
+}
--- src/main/java/XUnitTest.java
+++
@@ -1,7 +0,0 @@
-package main.java;
-
-public class XUnitTest {
-	public static void main(String args[]) {
-	    
-	}
-}
